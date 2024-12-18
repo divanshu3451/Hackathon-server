1bb69aad82a7c119ea9d5c600c45cc3faf9e52ef
divanshu3451 <awesomedev3451@gmail.com>
2024-12-18 21:52:26 +0530

    feat: installed necessary dependencies in node

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import cors from "cors";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import http from "http";
 
 const app = express();
 
 const PORT = 3000;
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+app.use(cors({ credentials: true }));
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+  server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
