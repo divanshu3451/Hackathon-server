0b5dc045fc4e55db6412cd3c8892c61c38e5cb40
Prakash Rathour <prakashkumarrathour247@gmail.com>
2024-12-18 20:05:46 +0530

    feat:  creates drizzle setup config

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,15 @@
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
 
-const db = drizzle(process.env.DATABASE_URL!);
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || '5432'),  
+});
+export const db = drizzle(pool);
